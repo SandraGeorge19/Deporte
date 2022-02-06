5cacf715dcffe2c06a8b8a6989b46ee1cf92b44e
Abdelwahab Zayed <ammz1997@gmail.com>
2022-02-06 03:52:01 -0800

    upcoming vc

--- Deporte/Module/LeagueDetails/View/UpComingEvents/UpComingEventCollectionViewCell.swift
+++ Deporte/Module/LeagueDetails/View/UpComingEvents/UpComingEventCollectionViewCell.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class UpComingEventCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var homeTeamLabel: UILabel!
     @IBOutlet weak var awayTeamLabel: UILabel!
     @IBOutlet weak var leagueNameLabel: UILabel!
     @IBOutlet weak var roundLabel: UILabel!
-    @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var homeTeamImageView: UIImageView!
     @IBOutlet weak var awayTeamImageView: UIImageView!
     override func awakeFromNib() {
